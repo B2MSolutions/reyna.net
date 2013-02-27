be1a4ae6311d23b2314cc2d6bebb64711595ca3d
sjwood <octocat@nym.hush.com>
2013-02-27 10:09:04 +0000

    Removed unneeded librarys from projects. Added build bat file

--- build/build.bat
+++ build/build.bat
@@ -1,1 +1,3 @@
-echo Hi from build file!
+SET NETFX_PATH=C:\Windows\Microsoft.NET\Framework\v3.5
+
+%NETFX_PATH%\MSBuild.exe %~dp0\..\src\reyna.sln /property:Platform="Any CPU" /property:Configuration=Release
