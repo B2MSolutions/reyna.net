36ae2c5cab94eb8b54796396fb988de6977a38c8
sjwood <octocat@nym.hush.com>
2013-02-27 10:14:25 +0000

    quoted path with space

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 SET NETFX_PATH=C:\Windows\Microsoft.NET\Framework\v3.5
 
-%NETFX_PATH%\MSBuild.exe %~dp0\..\src\reyna.sln /property:Platform="Any CPU" /property:Configuration=Release
+%NETFX_PATH%\MSBuild.exe "%~dp0..\src\reyna.sln" /property:Platform="Any CPU" /property:Configuration=Release
