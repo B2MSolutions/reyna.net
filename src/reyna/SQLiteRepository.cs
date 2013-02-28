2ad19f7a1729bed3882a9257b65812405fec5b8a
sjwood <octocat@nym.hush.com>
2013-02-28 10:13:03 +0000

    change projects to ccomile against X86 and refactor SQLiteRepository

--- src/reyna/SQLiteRepository.cs
+++ src/reyna/SQLiteRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace reyna
 {
     using System;
+    using System.Data.Common;
     using System.Data.SQLite;
     using System.IO;
     using System.Net;
@@ -9,8 +10,6 @@
 
     public class SQLiteRepository : IRepository
     {
-        private const string TableCreationSql = "CREATE TABLE Message (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, body TEXT);CREATE TABLE Header (id INTEGER PRIMARY KEY AUTOINCREMENT, messageid INTEGER, key TEXT, value TEXT, FOREIGN KEY(messageid) REFERENCES message(id));";
-
         public bool DoesNotExist(string name)
         {
             return !File.Exists(name);
@@ -20,15 +19,19 @@
         {
             SQLiteConnection.CreateFile(name);
 
-            this.ExecuteNonQuery(SQLiteRepository.TableCreationSql);
+            this.DatabaseAction((t) =>
+                {
+                    this.ExecuteNonQuery("CREATE TABLE Message (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, body TEXT);CREATE TABLE Header (id INTEGER PRIMARY KEY AUTOINCREMENT, messageid INTEGER, key TEXT, value TEXT, FOREIGN KEY(messageid) REFERENCES message(id));", t);
+                    t.Commit();
+                });
         }
 
         public IMessage Enqueue(IMessage message)
         {
-            return this.ActOnDatabase((t) =>
+            return this.DatabaseFunc((t) =>
                 {
                     var sql = this.CreateInsertSql(message);
-                    var messageId = Convert.ToInt32(this.ExecuteScalar(sql, t));
+                    var messageId = Convert.ToInt32(this.ExecuteScalar(this.CreateInsertSql(message), t));
 
                     sql = this.CreateInsertSql(messageId, message.Headers);
                     this.ExecuteScalar(sql, t);
@@ -42,6 +45,21 @@
 
         public IMessage Peek()
         {
+            // TODO
+            // complete refactoring.
+
+            //return this.DatabaseFunc((t) =>
+            //    {
+            //        var sql = this.CreateSelectTop1Sql();
+            //        using (var reader = this.ExecuteReader(sql, t))
+            //        {
+            //        }
+
+            //        t.Commit();
+
+            //        return Message;
+            //    });
+
             IMessage message = null;
 
             using (var connection = new SQLiteConnection("Data Source=reyna.db"))
@@ -81,6 +99,21 @@
 
         public IMessage Dequeue()
         {
+            //return this.ActOnDatabase((t) =>
+            //{
+                //var sql = this.CreateInsertSql(message);
+                //var messageId = Convert.ToInt32(this.ExecuteScalar(sql, t));
+
+                //sql = this.CreateInsertSql(messageId, message.Headers);
+                //this.ExecuteScalar(sql, t);
+
+                //var clone = this.AssignIdTo(message, messageId);
+
+                //t.Commit();
+                //return clone;
+            //});
+
+
             // TODO: encapsulate these 2 calls in a transaction?
             var message = this.Peek();
             if (message == null)
@@ -88,10 +121,20 @@
                 return null;
             }
 
-            var sql = string.Format("DELETE FROM Header WHERE messageid = {0};DELETE FROM Message WHERE id = {0}", message.Id);
-            this.ExecuteNonQuery(sql);
-            
-            return message;
+            return this.DatabaseFunc((t) =>
+                {
+                    // TODO: roll the above code into this transaction
+
+                    var sql = this.CreateDeleteSql(message);
+                    this.ExecuteNonQuery(sql, t);
+                    t.Commit();
+                    return message;
+                });
+        }
+
+        private string CreateSelectTop1Sql()
+        {
+            return "SELECT * FROM Message ORDER BY id ASC LIMIT 1";
         }
 
         private string CreateInsertSql(IMessage message)
@@ -111,43 +154,75 @@
             return builder.ToString();
         }
 
+        private string CreateDeleteSql(IMessage message)
+        {
+            return string.Format("DELETE FROM Header WHERE messageid = {0};DELETE FROM Message WHERE id = {0}", message.Id);
+        }
+
         private IMessage AssignIdTo(IMessage message, int id)
         {
+            // TODO - remove Cloneable interface from Message. this is the only place where we need it.
             var clone = message.Clone() as Message;
             clone.Id = id;
 
             return clone;
         }
 
-        private int ExecuteNonQuery(string sql)
+        private DbConnection CreateConnection()
         {
-            using (var connection = new SQLiteConnection("Data Source=reyna.db"))
+            var connection = new SQLiteConnection("Data Source=reyna.db");
+            connection.Open();
+            return connection;
+        }
+
+        private DbCommand CreateCommand(string sql, DbTransaction transaction)
+        {
+            var command = transaction.Connection.CreateCommand();
+            command.CommandText = sql;
+            command.Transaction = transaction;
+            command.Connection = transaction.Connection;
+            return command;
+        }
+
+        private int ExecuteNonQuery(string sql, DbTransaction transaction)
+        {
+            using (var command = this.CreateCommand(sql, transaction))
             {
-                connection.Open();
-                using (var command = new SQLiteCommand(sql, connection))
-                {
-                    return command.ExecuteNonQuery();
-                }
+                return command.ExecuteNonQuery();
             }
         }
 
-        private object ExecuteScalar(string sql, SQLiteTransaction transaction)
+        private object ExecuteScalar(string sql, DbTransaction transaction)
         {
-            using (var command = new SQLiteCommand(sql, transaction.Connection, transaction))
+            using (var command = this.CreateCommand(sql, transaction))
             {
                 return command.ExecuteScalar();
             }
         }
 
-        private IMessage ActOnDatabase(Func<SQLiteTransaction, IMessage> func)
+        private DbDataReader ExecuteReader(string sql, DbTransaction transaction)
         {
-            using (var connection = new SQLiteConnection("Data Source=reyna.db"))
+            using (var command = this.CreateCommand(sql, transaction))
             {
-                connection.Open();
-                using (var transaction = connection.BeginTransaction())
-                {
-                    return func(transaction);
-                }
+                return command.ExecuteReader();
+            }
+        }
+
+        private IMessage DatabaseFunc(Func<DbTransaction, IMessage> func)
+        {
+            using (var connection = this.CreateConnection())
+            using (var transaction = connection.BeginTransaction())
+            {
+                return func(transaction);
+            }
+        }
+
+        private void DatabaseAction(Action<DbTransaction> action)
+        {
+            using (var connection = this.CreateConnection())
+            using (var transaction = connection.BeginTransaction())
+            {
+                action(transaction);
             }
         }
     }
