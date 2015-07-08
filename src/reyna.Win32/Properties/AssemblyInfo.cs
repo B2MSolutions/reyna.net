fec50f9ce3d35701c509c5ed9147179570d8596b
Youhana-Hana <meyouhana@gmail.com>
2015-07-08 13:03:32 +0100

    [#95140930] updated version to 2.9.0.43

--- src/reyna.Win32/Properties/AssemblyInfo.cs
+++ src/reyna.Win32/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.0.42")]
-[assembly: AssemblyFileVersion("2.8.0.42")]
+[assembly: AssemblyVersion("2.9.0.43")]
+[assembly: AssemblyFileVersion("2.9.0.43")]
 [assembly: InternalsVisibleTo("reyna.Facts")]
 [assembly: InternalsVisibleTo("reyna.Integration.Facts")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
