73329efec68c92cdf01138df5f659aea0fe942a3
youhana-Hana <meyouhana@gmail.com>
2015-12-03 14:33:44 +0000

    added Batch

---
+++ src/reyna.Facts/GivenABatch.cs
@@ -0,0 +1,99 @@
+﻿namespace Reyna.Facts
+{
+    using System;
+    using Xunit;
+
+    public class GivenABatch
+    {
+        [Fact]
+        public void WhenConstructingShouldNotThrow()
+        {
+            var batch = new Batch();
+            Assert.NotNull(batch);
+            Assert.NotNull(batch.Events);
+        }
+
+        [Fact]
+        public void WhenCallingAddShouldAddNewMessage()
+        {
+            var batchMessage = new BatchMessage(1, "http://post.com", "payload");
+            var batch = new Batch();
+            batch.Add(batchMessage);
+
+            Assert.Equal(1, batch.Events.Count);
+            var message = batch.Events[0];
+            Assert.Equal("http://post.com", message.Url.ToString());
+            Assert.Equal(1, message.ReynaId);
+            Assert.Equal("payload", message.Payload);
+        }
+
+        [Fact]
+        public void WhenRemoveLastMessageAndOnlyOneMessageAvailableShouldNotRemoveIt()
+        {
+            var batchMessage = new BatchMessage(1, "http://post.com", "payload");
+            var batch = new Batch();
+            batch.Add(batchMessage);
+
+            batch.RemoveLastMessage();
+
+            Assert.Equal(1, batch.Events.Count);
+            var message = batch.Events[0];
+            Assert.Equal("http://post.com", message.Url.ToString());
+            Assert.Equal(1, message.ReynaId);
+            Assert.Equal("payload", message.Payload);
+        }
+
+        [Fact]
+        public void WhenRemoveLastMessageShouldRemoveIt()
+        {
+            var batchMessage = new BatchMessage(1, "http://post.com", "payload");
+            var batch = new Batch();
+            batch.Add(batchMessage);
+            batchMessage = new BatchMessage(2, "http://post.com2", "payload2");
+            batch.Add(batchMessage);
+            batchMessage = new BatchMessage(3, "http://post.com3", "payload3");
+            batch.Add(batchMessage);
+
+            batch.RemoveLastMessage();
+
+            Assert.Equal(2, batch.Events.Count);
+            var message = batch.Events[0];
+            Assert.Equal("http://post.com", message.Url.ToString());
+            Assert.Equal(1, message.ReynaId);
+            Assert.Equal("payload", message.Payload);
+            
+            message = batch.Events[1];
+            Assert.Equal("http://post.com2", message.Url.ToString());
+            Assert.Equal(2, message.ReynaId);
+            Assert.Equal("payload2", message.Payload);
+        }
+        
+        [Fact]
+        public void WhenCallingToJsonShouldReturnExpected()
+        {
+            var payload1 = "{\"Version Incremental\":\"20150514.093204\",\"User\":\"user\",\"Brand\":\"os\",\"device\":\"device\",\"Network Operator Name\":\"unknown\",\"Manufacturer\":\"MM\",\"updated\":1448534970738,\"list\":[{\"name\":\"System\",\"version\":\"4.1.1\",\"flag\":true},{\"name\":\"backupcon\",\"version\":\"4.1.1\",\"flag\":false}],\"Version Release\":\"4.1.1\",\"MAC\":\"MAC\"}";
+            var batchMessage1 = new BatchMessage(12, "http://post.com", payload1);
+
+            var payload2 = "{\"Brand\":\"os\",\"device\":\"device\",\"Network Operator Name\":\"unknown\",\"Manufacturer\":\"MM\",\"updated\":1448534970738,\"list\":[{\"name\":\"System\",\"version\":\"4.1.1\",\"flag\":true},{\"name\":\"backupcon\",\"version\":\"4.1.1\",\"flag\":false}],\"Version Release\":\"4.1.1\",\"MAC\":\"MAC\"}";
+            var batchMessage2 = new BatchMessage(14, "http://post2.com", payload2);
+
+            var batch = new Batch();
+            batch.Add(batchMessage1);
+            batch.Add(batchMessage2);
+
+            var expected = "{\"events\":[{\"url\":\"http://post.com\", \"reynaId\":12, \"payload\":{\"Version Incremental\":\"20150514.093204\",\"User\":\"user\",\"Brand\":\"os\",\"device\":\"device\",\"Network Operator Name\":\"unknown\",\"Manufacturer\":\"MM\",\"updated\":1448534970738,\"list\":[{\"name\":\"System\",\"version\":\"4.1.1\",\"flag\":true},{\"name\":\"backupcon\",\"version\":\"4.1.1\",\"flag\":false}],\"Version Release\":\"4.1.1\",\"MAC\":\"MAC\"}}, {\"url\":\"http://post2.com\", \"reynaId\":14, \"payload\":{\"Brand\":\"os\",\"device\":\"device\",\"Network Operator Name\":\"unknown\",\"Manufacturer\":\"MM\",\"updated\":1448534970738,\"list\":[{\"name\":\"System\",\"version\":\"4.1.1\",\"flag\":true},{\"name\":\"backupcon\",\"version\":\"4.1.1\",\"flag\":false}],\"Version Release\":\"4.1.1\",\"MAC\":\"MAC\"}}]}";
+            var actual = batch.ToJson();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void WhenCallingToJsonWithEmptyEventsShouldReturnExpected()
+        {
+            var batch = new Batch();
+            
+            var expected = "{\"events\":[]}";
+            var actual = batch.ToJson();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/reyna/MessageProvider/Batch.cs
@@ -0,0 +1,50 @@
+﻿namespace Reyna
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class Batch
+    {
+        public Batch()
+        {
+            this.Events = new List<BatchMessage>();
+        }
+
+        internal List<BatchMessage> Events { get; set; }
+
+        public void Add(BatchMessage message)
+        {
+            this.Events.Add(message);
+        }
+
+        public void RemoveLastMessage()
+        {
+            if (this.Events.Count > 1)
+            {
+                this.Events.RemoveAt(this.Events.Count - 1);
+            }
+        }
+
+        public string ToJson()
+        {
+            var buffer = new StringBuilder();
+            buffer.Append("{");
+            buffer.Append("\"events\"");
+            buffer.Append(":[");
+
+            if (this.Events.Count > 0)
+            {
+                foreach (var item in this.Events)
+                {
+                    buffer.Append(item.ToJson());
+                    buffer.Append(", ");
+                }
+
+                buffer = buffer.Remove(buffer.Length - 2, 2);
+            }
+
+            buffer.Append("]}");
+            return buffer.ToString();
+        }
+    }
+}
