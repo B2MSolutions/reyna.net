8164948f5e8299613f00873f619fa9a145abcacb
youhana-Hana <meyouhana@gmail.com>
2016-04-13 15:58:57 +0100

    exclude bluetooth from being detected as WIFI network

--- src/reyna/Properties/AssemblyInfo.cs
+++ src/reyna/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.10.0.48")]
+[assembly: AssemblyVersion("2.11.0.49")]
 [assembly: InternalsVisibleTo("Reyna.Integration.Facts.dll")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AcceptanceTests")]
