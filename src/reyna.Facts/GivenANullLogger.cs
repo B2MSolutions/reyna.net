bf85dd09c68470ec0fed834aa83bfab6b32fec7b
youhana-Hana <meyouhana@gmail.com>
2016-04-21 09:28:07 +0100

    fix coverage and increase version

---
+++ src/reyna.Facts/GivenANullLogger.cs
@@ -0,0 +1,32 @@
+﻿namespace Reyna.Facts
+{
+    using Xunit;
+    
+    public class GivenANullLogger
+    {
+        public GivenANullLogger()
+        {
+            this.Logger = new NullLogger();
+        }
+
+        private NullLogger Logger { get; set; }
+
+        [Fact]
+        public void WhenConstructingShouldNotThrow()
+        {
+            Assert.NotNull(this.Logger);
+        }
+
+        [Fact]
+        public void WhenCallingLoggingMethodsShouldNotThrow()
+        {
+            this.Logger.Debug("test {0}", 1);
+            this.Logger.Err("test {0}", 2);
+            this.Logger.Warn("test {0}", 3);
+            this.Logger.Info("test {0}", 4);
+            this.Logger.Verbose("test {0}", 5);
+            this.Logger.ToggleVerbose(true, "localhost");
+            this.Logger.Dispose();
+        }
+    }
+}
--- src/reyna.Win32/Properties/AssemblyInfo.cs
+++ src/reyna.Win32/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.0.50")]
-[assembly: AssemblyFileVersion("2.12.0.50")]
+[assembly: AssemblyVersion("2.13.0.51")]
+[assembly: AssemblyFileVersion("2.13.0.51")]
 [assembly: InternalsVisibleTo("reyna.Facts")]
 [assembly: InternalsVisibleTo("reyna.Integration.Facts")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
